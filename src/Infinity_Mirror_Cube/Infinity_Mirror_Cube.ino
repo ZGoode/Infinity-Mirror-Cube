83b6d299b8a3167f9d211370e1ffa557da244e35
Zachary Nathan Goode <zachary@the-goodes.com>
2018-12-16 15:12:47 -0800

    update

--- src/Infinity_Mirror_Cube/Infinity_Mirror_Cube.ino
+++ src/Infinity_Mirror_Cube/Infinity_Mirror_Cube.ino
@@ -8,16 +8,32 @@
 
 #include "patterns.h"
 
+#define buttonPin 4
+
+int buttonState;
+int lastButtonState = LOW;
+
+long lastDebounceTime = 0;
+long debounceDelay = 50;
+
 int currentPattern = 0;
 
 void setup() {
+  pinMode(buttonPin, INPUT);
   initializePixels();
 }
 
 void loop() {
   if (currentPattern == 0) {
-    
+    rainbowCycle0();
   } else if (currentPattern == 1) {
-    
+    rainbowCycle1();
   }
+  
+  int reading = digitalRead(buttonPin);
+
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  } 
 }
